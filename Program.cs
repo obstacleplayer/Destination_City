30102bb1a5d0c6109c0f8e66833b28c1f7fae63a
ShellGratuit <ademderbal88@gmail.com>
2020-12-09 22:12:27 +0100

    Algo

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Destination_City
+{
+    class Program
+    {
+         
+
+        static void Main(string[] args)
+        {
+            IList<IList<string>> pathss = new List<IList<string>>();
+
+            pathss.Add(new List<string> {"London", "New York"});
+            pathss.Add(new List<string> {"New York", "Lima"});
+            pathss.Add(new List<string> {"Lima", "Sao Paulo"});
+            
+            string DestCity(IList<IList<string>> paths)
+            {
+                string destination = string.Empty;
+
+                HashSet<string> hash = new HashSet<string>();
+                foreach (var citys in paths)
+                {
+                    string start = citys[0];
+
+                    if (hash.Contains(start) == false)
+                    {
+                        hash.Add(start);
+                    }
+                }
+
+                foreach (IList<string> citys in paths)
+                {
+                    string end = citys[1];
+
+                    if (hash.Contains(end) == false)
+                    {
+                        destination = end;
+                        break;
+                    }
+                }
+
+                return destination;
+            
+            
+            }
+            
+            Console.WriteLine($"{DestCity(pathss)}");
+           
+        }
+    }
+}
